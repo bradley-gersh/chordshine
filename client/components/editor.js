ee1ae2e3757e58f6881210cbf9b7d16c5c46c738
Brad Gersh <bradley.gersh@gmail.com>
2022-02-14 11:54:49 +0100

    feat: polysynth now plays chords

--- client/components/editor.js
+++ client/components/editor.js
@@ -1,18 +1,35 @@
 import React, { useState } from "react";
+import * as Tone from "tone";
 
 import { ClickableGrandStaff, Toolbar } from "./index";
 
+console.log("fire!");
+const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+
 const Editor = () => {
   const [noteList, setNoteList] = useState([]);
+  const [soundStarted, setSoundStarted] = useState(false);
 
-  const toggleNote = (noteStr) => {
+  const toggleNote = async (noteStr) => {
+    if (!soundStarted) {
+      setSoundStarted(true);
+      await Tone.start();
+    }
+
     const loc = noteList.indexOf(noteStr);
     const newNoteList =
       loc > -1
         ? noteList.slice(0, loc).concat(noteList.slice(loc + 1))
         : [...noteList, noteStr];
     setNoteList(newNoteList);
-    console.log("note toggled: " + noteStr);
+
+    if (loc > -1) {
+      synth.triggerRelease(noteStr);
+      console.log("note toggled off: " + noteStr);
+    } else {
+      synth.triggerAttack(noteStr);
+      console.log("note toggled on: " + noteStr);
+    }
   };
 
   return (
---
+++ client/components/singer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import * as Tone from "tone";
+
+const Singer = ({ noteList }) => {
+  return <div></div>;
+};
+
+Singer.propTypes = {
+  noteList: PropTypes.array.isRequired,
+};
+
+export default Singer;
