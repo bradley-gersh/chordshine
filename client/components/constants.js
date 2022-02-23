7366a406cd9f07e7ee1ce6120b15e004c3319c08
Brad Gersh <bradley.gersh@gmail.com>
2022-02-23 16:01:57 +0100

    test: changed rhyhtm presets

--- client/components/constants.js
+++ client/components/constants.js
@@ -1,5 +1,9 @@
 const MIN_VOL = -50; // dB range is -50 to 0.
-const RAMP_TIME_SEC = 2; // (sec) 1 second
-const CHANGE_TIME_MSEC = 6000; // (msec) 3 seconds
+const RAMP_TIME_SEC = 0.05; // (sec) 1 second
+const CHANGE_TIME_MSEC = 300; // (msec) 3 seconds
 
 export { MIN_VOL, RAMP_TIME_SEC, CHANGE_TIME_MSEC };
+
+// suggested settings:
+// 1. Slow waves: RTS = 2, CTM = 6000
+// 2. Rhythmic dance: RTS = 0.3, CTM = 500. 0.2/300 a little livelier.
