72abd1d4945c7cc26a19fead6faa76dd924f0bc1
Brad Gersh <bradley.gersh@gmail.com>
2022-04-12 17:20:11 +0200

    fix: corrected port for server on heroku

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const server = require("./server.js");
 
 const init = async () => {
   try {
     server.listen(PORT, () => {
-      console.log(`Server listening on http://localhost:${PORT}/`);
+      console.log(`Server listening on port ${PORT}/`);
     });
   } catch (err) {
     console.log(`[ERROR] Error in launching server:`, err);
