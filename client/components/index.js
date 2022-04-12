19cfde3d86c44a9d4ed7c56ad83d090f47153454
Brad Gersh <bradley.gersh@gmail.com>
2022-04-12 18:51:45 +0200

    build: case changes updated for heroku build in components file

--- client/components/index.js
+++ client/components/index.js
@@ -1,5 +1,5 @@
-export { default as Editor } from "./Editor.js";
-export { default as ClickableGrandStaff } from "./Staff.js";
-export { default as Toolbar } from "./Toolbar.js";
-export { default as SplashModal } from "./SplashModal.js";
-export { default as Note } from "./Note.js";
+export { default as Editor } from "./editor.js";
+export { default as ClickableGrandStaff } from "./staff.js";
+export { default as Toolbar } from "./toolbar.js";
+export { default as SplashModal } from "./splashModal.js";
+export { default as Note } from "./note.js";
